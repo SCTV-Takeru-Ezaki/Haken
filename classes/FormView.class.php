a6ee375080e7fee7247d29ff0aaa39d050aeeac4
hiroyukikobayashi <hiroyuki.kb.arroz@gmail.com>
2014-07-16 15:50:18 +0900

    CSS修正、class/FormView.class.php修正など
    ・uploads/追加
    ・img/追加
    ・css/style.css,ie9.css修正、追加
    ・.htaccess追加
    class/FormView.class.phpのcreateFormView()のcase 'HIDDEN':を修正

--- classes/FormView.class.php
+++ classes/FormView.class.php
@@ -331,9 +331,11 @@ class FormView{
 				case 'HIDDEN':
 					$style = $this->createStyle();
 					$value = (!empty($this->model->postData[$enq['NAME']]))? $this->model->postData[$enq['NAME']] : "";
+					$value = (!empty($enq['PROPS']['value']))? $enq['PROPS']['value'] : "";
 					$tag = "<input type=\"{$enq['TYPE']}\" name=\"{$enq['NAME']}\" style=\"{$style}\" value=\"{$value}\">\n";
 					$html = str_get_html($tag, true, true, DEFAULT_TARGET_CHARSET, false);
 					$html->find('input',0)->value = $this->model->getPostedValueFromKey($enq['NAME']);
+					$html->find('input',0)->value = (!empty($enq['PROPS']['value'][0]))? $enq['PROPS']['value'][0] : "";
 					$tag = $html->find('input',0);
 					break;
 				default:
